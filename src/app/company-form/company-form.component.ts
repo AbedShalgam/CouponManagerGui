4bc45bf6b0745da25ef068760591af56d65cd8e8
Gazi <gazisamoni@gmail.com>
2021-03-14 17:52:21 +0200

    bidsnin

---
+++ src/app/company-form/company-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../Entities/company';
+import { AdminService } from '../Services/admin.services';
+import { NgForm } from '@angular/forms';
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Coupon } from '../Entities/coupon';
+
+@Component({
+  selector: 'company-form',
+  templateUrl: './company-form.component.html',
+  styleUrls: ['./company-form.component.css']
+})
+export class CompanyFormComponent implements OnInit {
+  copon: Coupon;
+  company: Company = {id:0,name:'test',email:'e@e.com',password:'ps',coupons:{copon}}
+
+  constructor(private adminService:AdminService) { }
+
+  ngOnInit(): void {
+  }
+  apply(companyForm: NgForm): void
+  {
+    //to check
+    console.log("before");
+    console.log(companyForm.value);
+    
+    const formValue = companyForm.value;
+    const name= formValue.name;
+    const email= formValue.email;
+    const password = formValue.value;
+    console.log("after");
+    //need to fix 
+     this.company.name =formValue.name;
+     this.company.email = formValue.email;
+     this.company.password = formValue.value;
+
+    this.adminService.addCompany(this.company);
+  }
+
+}
