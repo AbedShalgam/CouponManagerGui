16a162b8bbf31bc8d61346bf9e628518f163d172
abed <abed.shalgam@gmail.com>
2021-03-14 21:30:21 +0200

    other fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     CouponComponent,
     CompanyUserComponent,
     CustomerUserComponent,
-    CompanyFormComponent
+    CompanyFormComponent,
   ],
   imports: [
     BrowserModule,
